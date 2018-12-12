aa0a4028483bb89a10c36f98643f26804ac0d2f3
Holger Ludvigsen <holger.ludvigsen@bekk.no>
2018-12-12 16:08:29 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+console.log("script lastet")
+
 const button = document.getElementById("beregn");
 
 const output = document.getElementById("output");
@@ -11,6 +13,9 @@ const fetchData = {
 }
 
 fetch("https://9f22opit6e.execute-api.us-east-2.amazonaws.com/default/reisefradrag", fetchData)
-  .then(function() {
-      output.innerHTML = JSON.stringify(response.json());
-  });
+  .then(function(response) {
+    console.log(response);
+    output.innerHTML = JSON.stringify(response.json());
+  }).fail(function() {
+    console.log(e);
+});
